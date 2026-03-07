a7b6b201f95e25a0ad4583c63e3d2663e0dc97cc
m2gi-deschamt <omtd.deschamps@gmail.com>
2026-03-07 10:35:56 +0100

    first vector impl

---
+++ fun/vectorImpl.cpp
@@ -0,0 +1,44 @@
+/*
+The idea is to implement vector object
+- a vector have a dynamic size
+- it can be access by index
+- push & pop, on the last element
+- copy data in a bigger space if needed
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+typedef struct {
+    int* data;
+    int length=0;
+    int capacity;
+} Vector;
+
+
+void append(Vector* vector, int value) {
+    if(vector->length >= vector->capacity) {
+        if(vector->capacity == 0) 
+            vector->capacity +=4;
+        else 
+            vector->capacity *=2;
+        vector->data = (int*) realloc(vector->data, vector->capacity * sizeof(*vector->data));
+    };
+    vector->data[vector->length++] = value;
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+    Vector vec = {0};
+    for (size_t i = 0; i < 10; i++) {
+        append(&vec,i);
+    };
+
+    for (size_t i = 0; i < 10; i++) {
+        std::cout << " " << vec.data[i] << " ";
+    };
+}
